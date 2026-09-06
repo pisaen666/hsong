632f47c64b59e552c88a93f7aab349951e807c77
pisaen666 <pisaen666@users.noreply.github.com>
2026-09-06 10:21:03 +0700

    fix: bypass browser cache with script version v8.1 and add inline instant localStorage purge

--- server.js
+++ server.js
@@ -37,7 +37,12 @@ const server = http.createServer((req, res) => {
                 res.end(`Server Error: ${err.code}`);
             }
         } else {
-            res.writeHead(200, { 'Content-Type': contentType });
+            res.writeHead(200, {
+                'Content-Type': contentType,
+                'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
+                'Pragma': 'no-cache',
+                'Expires': '0'
+            });
             res.end(content);
         }
     });
